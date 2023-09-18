7c598627dcbd5577075ad3668c3a4e2094e71923
muhammadhuda22 <145311965+muhammadhuda22@users.noreply.github.com>
2023-09-18 19:19:05 +0700

    first commit

---
+++ PRAK002_2310817210017_MuhammadNurhudaBaihaqi.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void main () {
+
+    printf("Selamat Pagi, Muhammad Nurhuda Baihaqi\n");
+    printf("Selamat Siang, Muhammad Nurhuda Baihaqi\n");
+    printf("Selamat Malam, Muhammad Nurhuda Baihaqi\n");
+
+}
