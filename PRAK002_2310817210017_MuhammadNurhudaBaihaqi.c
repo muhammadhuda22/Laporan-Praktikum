16879d2d8c750441efbf09e5aaad1cd8595de62d
muhammadhuda22 <145311965+muhammadhuda22@users.noreply.github.com>
2023-09-19 09:27:20 +0700

    Delete PRAK002_2310817210017_MuhammadNurhudaBaihaqi.c

--- PRAK002_2310817210017_MuhammadNurhudaBaihaqi.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-void main () {
-
-    printf("Selamat Pagi, Muhammad Nurhuda Baihaqi\n");
-    printf("Selamat Siang, Muhammad Nurhuda Baihaqi\n");
-    printf("Selamat Malam, Muhammad Nurhuda Baihaqi\n");
-
-}
