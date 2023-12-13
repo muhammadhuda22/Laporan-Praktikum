67d1ba5580f59d9ac5830acf9feaa942a19ce084
muhammadhuda22 <145311965+muhammadhuda22@users.noreply.github.com>
2023-12-13 11:56:31 +0700

    Update PRAK103_2310817210017_MUHAMMADNURHUDABAIHAQI.py

--- modul1/Python/PRAK103_2310817210017_MUHAMMADNURHUDABAIHAQI.py
+++ modul1/Python/PRAK103_2310817210017_MUHAMMADNURHUDABAIHAQI.py
@@ -3,8 +3,8 @@ b = 6
 x = 10
 y = 7 
 hasil = (a + b) * x / y
-print("Variabel a bernilai {a}")
-print("Variabel b bernilai {b}")
-print("Variabel x bernilai {x}")
-print("Variabel y bernilai {y}")
-print("Hasil dari a ditambah b dikali x dibagi y adalah {hasil:.2f}")
+print(f"Variabel a bernilai {a}")
+print(f"Variabel b bernilai {b}")
+print(f"Variabel x bernilai {x}")
+print(f"Variabel y bernilai {y}")
+print(f"Hasil dari a ditambah b dikali x dibagi y adalah {hasil:.2f}")
