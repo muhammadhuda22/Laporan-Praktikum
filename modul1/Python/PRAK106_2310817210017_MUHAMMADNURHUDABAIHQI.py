13ff2c891108c8ee06379c75ff2625d979436ce2
muhammadhuda22 <145311965+muhammadhuda22@users.noreply.github.com>
2023-12-13 12:01:23 +0700

    Update PRAK106_2310817210017_MUHAMMADNURHUDABAIHQI.py

--- modul1/Python/PRAK106_2310817210017_MUHAMMADNURHUDABAIHQI.py
+++ modul1/Python/PRAK106_2310817210017_MUHAMMADNURHUDABAIHQI.py
@@ -4,12 +4,12 @@ c = 3
 A = 0 if a==b else 0
 B = 1 if b>c else 0
 C = 1 if a!=c else 0
-print("Variabel a bernilai", a)
-print("Variabel a bernilai", b)
-print("Variabel a bernilai", c)
-print("Apakah a sama dengan b ? jawabannya adalah", A)
-print("Apakah b lebih besar dari c ? jawabannya adalah", B)
-print("Apakah a tidak sama dengan c ? jawabannya adalah", C)
+print(f"Variabel a bernilai {a}")
+print(f"Variabel a bernilai {b}")
+print(f"Variabel a bernilai {c}")
+print(f"Apakah a sama dengan b ? jawabannya adalah {A}")
+print(f"Apakah b lebih besar dari c ? jawabannya adalah {B}")
+print(f"Apakah a tidak sama dengan c ? jawabannya adalah {C}")
 
     
 
