a17379f2785a449ae2567517a8b239392f36a547
muhammadhuda22 <145311965+muhammadhuda22@users.noreply.github.com>
2023-12-13 12:15:23 +0700

    Update PRAK204_2310817210017_MUHAMMADNURHUDABAIHAQI.py

--- modul2/Python/PRAK204_2310817210017_MUHAMMADNURHUDABAIHAQI.py
+++ modul2/Python/PRAK204_2310817210017_MUHAMMADNURHUDABAIHAQI.py
@@ -1,6 +1,6 @@
 jari_jari, tinggi, = map(float, input().split())
 volume = 22/7 *jari_jari*jari_jari*tinggi
-luas = 22/7 *jari_jari*(jari_jari+tinggi)
+luas = 2 * 22/7 * jari_jari * (jari_jari+tinggi)
 keliling = 2*(22/7)*jari_jari
 print(f"Volume = {volume:.2f}")
 print(f"Luas = {luas:.2f}")
