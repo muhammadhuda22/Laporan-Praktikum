4187d001dacaf71993e994b21259510836e88cc4
muhammadhuda22 <145311965+muhammadhuda22@users.noreply.github.com>
2023-12-13 12:06:28 +0700

    Update PRAK109_2310817210017_MUHAMMADNURHUDABAIHAQI.py

--- modul1/Python/PRAK109_2310817210017_MUHAMMADNURHUDABAIHAQI.py
+++ modul1/Python/PRAK109_2310817210017_MUHAMMADNURHUDABAIHAQI.py
@@ -1,6 +1,6 @@
 a = 958730
 b = 5
 jumlah = (a/b)
-print("Jumlah pasukan yang dibawa Yu Zhong =", a)
-print("Jumlah pahlawan =", b)
-print(f"Jumlah pasukan yang harus dikalahkan setiap pahlawan adalah {jumlah} pasukan")
+print(f"Jumlah pasukan yang dibawa Yu Zhong = {a}")
+print(f"Jumlah pahlawan = {b}")
+print(f"Jumlah pasukan yang harus dikalahkan setiap pahlawan adalah {jumlah:.0f} pasukan")
