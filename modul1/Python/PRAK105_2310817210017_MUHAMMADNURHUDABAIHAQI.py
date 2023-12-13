b932a044578b8527b9e1fccca1ccc3eff77b2c99
muhammadhuda22 <145311965+muhammadhuda22@users.noreply.github.com>
2023-12-13 11:58:53 +0700

    Update PRAK105_2310817210017_MUHAMMADNURHUDABAIHAQI.py

--- modul1/Python/PRAK105_2310817210017_MUHAMMADNURHUDABAIHAQI.py
+++ modul1/Python/PRAK105_2310817210017_MUHAMMADNURHUDABAIHAQI.py
@@ -5,8 +5,8 @@ y = 8
 ab = 9%5
 xy = 8%8
 total = ab+xy
-print("Variabel a bernilai", a)
-print("Variabel b bernilai", b)
-print("Variabel x bernilai", x)
-print("Variabel y bernilai", y)
-print("Total sisa hasil bagi dari a dibagi b dan x dibagi y adalah", total)
+print(f"Variabel a bernilai {a}")
+print(f"Variabel b bernilai {b}")
+print(f"Variabel x bernilai{x}" )
+print(f"Variabel y bernilai {y}" )
+print(f"Total sisa hasil bagi dari a dibagi b dan x dibagi y adalah {total}")
