0f45cc3ef1869ed16df2cc3e92df8a2dbb40372b
muhammadhuda22 <145311965+muhammadhuda22@users.noreply.github.com>
2023-12-13 11:55:19 +0700

    Update PRAK107_2310817210017_MUHAMMADNURHUDABAIHAQI.py

--- modul1/Python/PRAK107_2310817210017_MUHAMMADNURHUDABAIHAQI.py
+++ modul1/Python/PRAK107_2310817210017_MUHAMMADNURHUDABAIHAQI.py
@@ -5,9 +5,9 @@ keliling = a + b + c
 harga = 85000
 biaya = harga*keliling
 print("Diketahui :")
-print(f"Panjang sisi segitiga berturut-turut adalah {a},{b},dan{c}")
-print("Keliling tanah Pak Dengklek adalah", keliling)
-print("Harga tanah Per Meter adalah", harga)
+print(f"Panjang sisi segitiga berturut-turut adalah {a}, {b}, dan {c}")
+print(f"Keliling tanah Pak Dengklek adalah {keliling}")
+print(f"Harga tanah Per Meter adalah {harga}")
 print("Jawaban :")
-print("Biaya yang diperlukan Pak Dengklek adalah : Rp", biaya)
+print(f"Biaya yang diperlukan Pak Dengklek adalah : Rp{biaya}")
  
