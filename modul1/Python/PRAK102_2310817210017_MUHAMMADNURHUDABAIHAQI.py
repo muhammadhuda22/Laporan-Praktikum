a283b9b99347378f662d5f1eb7a10f76da177b75
muhammadhuda22 <145311965+muhammadhuda22@users.noreply.github.com>
2023-12-13 11:48:18 +0700

    Update PRAK102_2310817210017_MUHAMMADNURHUDABAIHAQI.py

--- modul1/Python/PRAK102_2310817210017_MUHAMMADNURHUDABAIHAQI.py
+++ modul1/Python/PRAK102_2310817210017_MUHAMMADNURHUDABAIHAQI.py
@@ -2,7 +2,7 @@ a = 4
 b = 8
 c = 3
 hasil = a*b/c
-print("Variabel a bernilai", a)
-print("Variabel b bernilai", b)
-print("Variabel c bernilai", c)
-print("Hasil dari a dikali b dibagi c adalah", hasil)
+print("Variabel a bernilai {a}" )
+print("Variabel b bernilai {b}")
+print("Variabel c bernilai {c}")
+print("Hasil dari a dikali b dibagi c adalah {hasil:.6f}")
